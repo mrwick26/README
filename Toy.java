b0ec378e001408763e8842528db5fec81d733cd8
manongnimo's <tesoreroangelo26@gmail.com>
2026-05-11 19:53:39 +0800

    Update Toy.java

--- Toy.java
+++ Toy.java
@@ -5,6 +5,6 @@ public class Toy {
     public int quantity;
 
     public static void main(String args[]){
-        System.out.println("for the toy class");
+        System.out.println("Hello Manongnimous");
     }
 }
