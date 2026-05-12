77563f9d52ea5352ba41a3d300ca4cb902c36d10
manongnimo's <tesoreroangelo26@gmail.com>
2026-05-12 04:10:20 +0000

    5/12/2026

--- Toy.java
+++ Toy.java
@@ -1,10 +1,37 @@
+
+/**
+ * @author 
+ * 
+ * 
+ * 
+ * manongnimo's
+ * 
+ * 
+ * 
+ * This a Toy Object that has a parameter of name, brand, price, quantity
+ * 
+ */
+
 public class Toy {
-    public String name;
+    public String name = "Mrwick";
     public String brand;
     public double price;
     public int quantity;
 
+    public static String name1 = "Gundam";
+
+    /* this
+    * is
+    * a
+    * multi
+    * line
+    * comment
+    */
+
+
     public static void main(String args[]){
         System.out.println("Hello Manongnimous");
+        Toy toy1 = new Toy();
+        System.out.println(toy1.name);
     }
 }
