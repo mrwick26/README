6455d6ae0bfe1de4db9888a4336ed40db0b6dbfe
manongnimo's <tesoreroangelo26@gmail.com>
2026-06-20 01:54:43 +0000

    Feat - Task3

---
+++ Tasks/Task3.java
@@ -0,0 +1,21 @@
+package Tasks;
+
+public class Task3 {
+    
+    public static void main(String[] args) {
+        
+        String a = new String("Wow");
+        String b = "Wow";
+        String c = a;
+        String d = c;
+
+        boolean b1 = a.equals(b);
+        boolean b2 = d.equals(b + "");
+        boolean b3 = c.equals(a);
+
+        if(b1 && b2 && b3)
+            System.out.println("Success");
+        else
+            System.out.println("Not Success");
+    }
+}
