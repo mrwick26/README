1cf658a6a3b214dda7174925a6f2446de7ad999c
manongnimo's <tesoreroangelo26@gmail.com>
2026-06-20 01:33:45 +0000

    Feat - Task2

--- Tasks/Task2.java
+++ Tasks/Task2.java
@@ -2,9 +2,18 @@ package Tasks;
 
 public class Task2 {
     
-
     public static void main(String[] args) {
-        int a = 0;
-        System.out.println("H3ll0 w0rld 2.0 true");
+        char a = 'H';
+        int b = 3;
+        String c = "ll";
+        short d = 0;
+        String e = "w";
+        byte f = 0;
+        String g = "rld";
+        float h = 2.0f;
+        boolean i = true;
+        String output = a + "" + b + c + d + " " + e + f + g + " " + h + " " + i;
+        System.out.println(output);
+
     }
 }
