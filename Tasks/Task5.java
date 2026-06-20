3b723a6801fb973c18d6174d66179b5db8d5210c
manongnimo's <tesoreroangelo26@gmail.com>
2026-06-20 03:01:30 +0000

    Feat - Task5

--- Tasks/Task5.java
+++ Tasks/Task5.java
@@ -9,14 +9,26 @@ public class Task5 {
         int firstNum = 0;
         int secondNum = 0;
         int thirdNum = 0;
-
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter three numbers:");
         firstNum = input.nextInt();
         secondNum = input.nextInt();
         thirdNum = input.nextInt();
 
-        System.out.println(firstNum + "" + secondNum + "" + thirdNum);
+        String largestNum = "The largest number is ";
+        System.out.println("First number: " + firstNum + "\n"
+            + "Second number: " + secondNum + "\n"
+            + "Third number: " + thirdNum);
 
+        if (firstNum == secondNum && secondNum == thirdNum) {
+            System.out.println("All numbers are equal");
+        }else if(firstNum >= secondNum && firstNum >= thirdNum){
+            System.out.println(largestNum + "the first number, which is: " + firstNum);
+        }else if (secondNum >= firstNum && secondNum >= thirdNum) {
+            System.out.println(largestNum + "the second number, which is: " + secondNum);
+        }else if (thirdNum >= firstNum && thirdNum >= secondNum) {
+            System.out.println(largestNum + "the third number, which is: " + thirdNum);
+        }
     }
 }
